cd3c6b10e099b0f289721fb9ae85a29c1c87a4b1
Juan Diego <juanndiiego2@gmail.com>
2025-02-22 19:20:55 -0500

    Agregar componente TextPressure y estilos asociados; ajustar configuración de Particles y estilos de la aplicación

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,60 @@
 import Particles from './components/Particles';
+import TextPressure from './components/TextPressure';
 
 function App() {
   return (
     <div
       style={{
         position: 'relative',
-        width: '100vw',
-        height: '100vh',
+        width: '100%',
+        height: '100%',
         backgroundColor: 'black',
         overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        top: "100px",
       }}
     >
-      {/*Fondo de partículas */}
+      {/* Fondo de partículas */}
       <Particles
         particleColors={['#ffffff', '#aaaaaa', '#8888ff', '#ff88ff']}
-        particleCount={5000}
+        particleCount={8000}
         particleSpread={20}
         speed={0.05}
         disableRotation={false}
       />
+
+      {/* Contenedor para los textos */}
+      <div style={{ textAlign: 'center' }}>
+        {/* Primer texto */}
+        <TextPressure
+          text="STAR WARS"
+          flex={true}
+          alpha={false}
+          stroke={true} 
+          width={true}
+          weight={true}
+          italic={true}
+          minFontSize={350} 
+          textColor="#FFD700"
+          strokeColor="#000000"
+        />
+
+        {/* Segundo texto */}
+        <TextPressure
+          text="EXPLORER"
+          flex={true}
+          alpha={false}
+          stroke={true} 
+          width={true}
+          weight={true}
+          italic={true}
+          minFontSize={350} 
+          textColor="#FFD700"
+          strokeColor="#000000"
+        />
+      </div>
     </div>
   );
 }
